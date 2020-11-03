259f9c0fb60a14c6a83a899dc66785c57c00c6e2
wend3ll-souza <wend3llsouza@gmail.com>
2020-11-02 22:15:00 -0300

    :rocket: Responsividade v2

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".hamburger").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+
+document.querySelector("#navMenu").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+})
